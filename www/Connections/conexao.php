758ceb136242411e545028fdf0f44167e5007dc2
Clerson Borges de Menezes <clersonbm82@hotmail.com>
2016-11-03 01:11:20 -0200

    Update conexao.php
    Mudança dos dados de conexão com o banco de dados online

--- www/Connections/conexao.php
+++ www/Connections/conexao.php
@@ -2,9 +2,9 @@
 # FileName="Connection_php_mysql.htm"
 # Type="MYSQL"
 # HTTP="true"
-$hostname_conexao = "localhost";
-$database_conexao = "u327012897_almox";
-$username_conexao = "root";
-$password_conexao = "";
+$hostname_conexao = "sql108.byethost9.com";
+$database_conexao = "b9_18777122_almox";
+$username_conexao = "b9_18777122";
+$password_conexao = "cbm6916";
 $conexao = @mysql_connect($hostname_conexao, $username_conexao, $password_conexao) or trigger_error(mysql_error(),E_USER_ERROR); 
 ?>
